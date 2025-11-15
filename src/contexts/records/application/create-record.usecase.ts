b93728438e43fc55444b97c08308da00492a03fe
Acidiney Dias <24493328+acidiney@users.noreply.github.com>
2025-11-15 18:57:37 +0000

    feat: ensure we don't have duplicated entries for records

--- src/contexts/records/application/create-record.usecase.ts
+++ src/contexts/records/application/create-record.usecase.ts
@@ -1,4 +1,8 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { ConflictException, Inject, Injectable } from '@nestjs/common';
+import {
+  RECORDS_READ_REPOSITORY,
+  RecordsReadRepository,
+} from '../domain/repositories/records-read.repository';
 import {
   RECORDS_REPOSITORY,
   RecordsRepository,
@@ -11,9 +15,21 @@ export class CreateRecordUseCase {
   constructor(
     @Inject(RECORDS_REPOSITORY)
     private readonly repo: RecordsRepository,
+    @Inject(RECORDS_READ_REPOSITORY)
+    private readonly readRepo: RecordsReadRepository,
   ) {}
 
   async execute(dto: CreateRecordInput): Promise<RecordOutput> {
+    const existing = await this.readRepo.findByUnique(
+      dto.artist,
+      dto.album,
+      dto.format,
+    );
+
+    if (existing) {
+      throw new ConflictException('Record already exists');
+    }
+
     const created = await this.repo.create(dto);
     return RecordOutput.fromModel(created);
   }
